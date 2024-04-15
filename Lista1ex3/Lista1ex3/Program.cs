462df4f5be607be5bf91471381aa779868f5d85f
EduardoElias-ifsp <166325459+EduardoElias-ifsp@users.noreply.github.com>
2024-04-15 20:18:10 -0300

    Update Program.cs

--- Lista1ex3/Lista1ex3/Program.cs
+++ Lista1ex3/Lista1ex3/Program.cs
@@ -19,8 +19,7 @@ namespace Lista1ex3
 
 
             area = diagonal * diagonal / 2;
-            Console.Write("A área do quadrado é: ");
-            Console.WriteLine(area);
+            Console.Write("A área do quadrado é: {0}",area);
         }
     }
 }
